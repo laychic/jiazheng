652cd884a19888cd5ea1609ff767a64a38472b9b
ArthurF <1332275321@qq.com>
2025-05-28 09:49:00 +0800

    增加了前后端连接的配置文件corsConfig.java

---
+++ back/src/main/java/com/dao/GuzhupingjiaDao.java
@@ -0,0 +1,28 @@
+package com.dao;
+
+import com.entity.GuzhupingjiaEntity;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import java.util.List;
+import java.util.Map;
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+
+import org.apache.ibatis.annotations.Param;
+import com.entity.vo.GuzhupingjiaVO;
+import com.entity.view.GuzhupingjiaView;
+
+
+public interface GuzhupingjiaDao extends BaseMapper<GuzhupingjiaEntity> {
+	
+	List<GuzhupingjiaVO> selectListVO(@Param("ew") Wrapper<GuzhupingjiaEntity> wrapper);
+	
+	GuzhupingjiaVO selectVO(@Param("ew") Wrapper<GuzhupingjiaEntity> wrapper);
+	
+	List<GuzhupingjiaView> selectListView(@Param("ew") Wrapper<GuzhupingjiaEntity> wrapper);
+
+	List<GuzhupingjiaView> selectListView(Pagination page,@Param("ew") Wrapper<GuzhupingjiaEntity> wrapper);
+	
+	GuzhupingjiaView selectView(@Param("ew") Wrapper<GuzhupingjiaEntity> wrapper);
+
+
+}
